ff618e661e19a433e769b4e24876759e3a2533ac
erick-augusto <erick_aug@hotmail.com>
2015-07-27 14:39:51 -0300

    BD atualizado com a tabela solicitações

--- BD.sql
+++ BD.sql
@@ -4,45 +4,45 @@ use trabalho;
 create table aluno(
 ra_aluno decimal(8),
 nome_aluno text,
--- password_aluno varchar(50),              password de login
 primary key(ra_aluno));
 
 insert into aluno (ra_aluno, nome_aluno) values (
 11111111,'Erick');
 insert into aluno (ra_aluno, nome_aluno) values (
-22222222,'Bob');
-select  * from aluno;
+33333333,'Augusto');
+-- select  * from aluno;
 
 create table docente(
 siape_docente decimal(6),
 nome_docente char(40),
--- password_docente varchar(50),           password de login
 primary key(siape_docente));
-insert into docente (siape_docente,nome_docente) values (
-111111,'Bob Sponja');
+
+insert into docente(siape_docente,nome_docente) values(
+222222,'Marcio');
 
 create table disciplina(
 cod_disciplina char(6),
-nome_disciplina char(50),
+nome_disciplina char(30),
 t integer,
 p integer,
 i integer,
-primary key(cod_disciplina));
+primary key(cod_disciplina, nome_disciplina));
 
 create table coordenador(
 siape_coordenador decimal(6),
 foreign key(siape_coordenador) references docente(siape_docente));
-insert into coordenador (siape_coordenador) values (
-111111);
+
+insert into coordenador(siape_coordenador)values(
+222222);
 
 create table turma(
 id_turma integer,
 cod_turma char(8),
 cod_disciplina char(6),
--- nome_disciplina char(50),
+nome_disciplina char(30),
 vagas integer,
 primary key(id_turma),
-foreign key(cod_disciplina) references disciplina(cod_disciplina));
+foreign key(cod_disciplina,nome_disciplina) references disciplina(cod_disciplina,nome_disciplina));
 -- foreign key(nome_disciplina) references disciplina(nome_disciplina));
 
 create table aula(
@@ -51,7 +51,7 @@ dia text,
 hini text,
 hfim text,
 sala text,
--- nome_docente char(40),
+nome_docente char(40),
 id_turma integer,
 primary key(id_aula),
 -- foreign key(nome_docente) references docente(nome_docente),
@@ -66,5 +66,8 @@ foreign key(ra_aluno) references aluno(ra_aluno),
 -- foreign key(nome_disciplina) references disciplina(nome_disciplina),
 foreign key(cod_turma) references turma(id_turma));
 
-
-
+create table solicitacoes(
+ra_aluno decimal(8),
+cod_turma integer,
+foreign key(ra_aluno) references aluno(ra_aluno),
+foreign key(cod_turma) references turma(id_turma));
